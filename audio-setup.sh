ca22e6d37ff8c5d3acc69b0fd49e45281f80ae56
Tom Kearns <linux54@riseup.net>
2024-09-22 17:35:39 +0200

    Added audio setup script

---
+++ audio-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Sun Sep-22-2024  17:29
+################################################################################
+# Author      : Tom Kearns
+# Github      : https://www.github.com/tommonad/My_install_scripts
+################################################################################
+# Installing pipewire for audio
+# https://wiki.archlinux.org/title/PipeWire
+sudo pacman -S --noconfirm --needed pipewire
+sudo pacman -S --noconfirm --needed lib32-pipewire
+sudo pacman -S --noconfirm --needed wireplumber
+sudo pacman -S --noconfirm --needed pipewire-alsa
+sudo pacman -S --noconfirm --needed pipewire-jack
+sudo pacman -S --noconfirm --needed lib32-pipewire-jack
+sudo pacman -S --noconfirm --needed pipewire-zeroconf
+sudo pacman -S --noconfirm --needed pipewire-pulse
+
+echo "Reboot now"
