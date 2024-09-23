c30e54df9df1cb52da3f60f2039cf8a7834d10fb
Tom Kearns <linux54@riseup.net>
2024-09-23 07:43:57 +0200

    Added pacman and aur install scripts

--- arch_package_install.sh
+++ arch_package_install.sh
@@ -21,11 +21,30 @@ set -e
 #tput setaf 8 = light blue
 ##################################################################################################################
 
+# Install arch packages
+package_file="arch_packages.txt"
+
+# Check if the file exists
+if [ ! -f "$package_file" ]; then
+  echo "Error: File $package_file not found!"
+  exit 1
+fi
+
+# Install the packages listed in the file using pacman
+sudo pacman -S --needed --noconfirm $(cat "$package_file")
+
+
+
+# Check if the file exists
+if [ ! -f "$package_file" ]; then
+  echo "Error: File $package_file not found!"
+  exit 1
+fi
 
 tput setaf 2
 echo
 echo "#########################################################################"
-echo "###                   ---  ---                    ###"
+echo "###                   --- Pacman is up to date ---                    ###"
 echo "#########################################################################"
 tput sgr0
 echo
