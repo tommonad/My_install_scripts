4c41cf84ff3880d57b46f850456b2460c78b5348
tommonad <linux54@riseup.net>
2025-08-06 17:19:03 +0200

    Changed the ip address of the printer

--- 07.printerinstall.sh
+++ 07.printerinstall.sh
@@ -4,9 +4,8 @@
 
 sudo pacman -S --noconfirm --needed cups cups-pdf gutenprint ghostscript gsfonts system-config-printer
 sudo systemctl enable --now cups.socket
-yay -S --noconfirm --needed brscan4 brother-dcp562dw
-sudo brsaneconfig4 -a name="BrotherScanner" model="DCP-J562DW" ip=192.168.1.105
-
+yay -S --noconfirm --needed brscan4 brother-dcpj562dw
+sudo brsaneconfig4 -a name="BrotherScanner" model="DCP-J562DW" ip=192.168.1.5
 
 # Turn on printer and then execute the following command
-#  lpadmin -p myprinter -E -v "ipp://192.168.1.105/ipp/print" -m everywhere
+#  lpadmin -p myprinter -E -v "ipp://192.168.1.05/ipp/print" -m everywhere
